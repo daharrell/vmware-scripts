7ec76794ae0ba0ce007dd18d12ec2c1494ede0c4
daharrell <daharrell@users.noreply.github.com>
2017-07-09 14:24:24 -0400

    Update disable_nic_by_OS_ type.ps1

--- disable_nic_by_OS_ type.ps1
+++ disable_nic_by_OS_ type.ps1
@@ -1,1 +1,2 @@
-﻿Get-VM | Where{$_.Guest.OSFullName -match 'windows'}|Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -Confirm:$false
+Connect-VIServer
+Get-VM | Where{$_.Guest.OSFullName -match 'windows'}|Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -Confirm:$false
