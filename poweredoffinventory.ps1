247ab9f560d62687e3ec2d0666c9182c11d92f3e
daharrell <daharrell@users.noreply.github.com>
2017-07-09 15:15:20 -0400

    Inventory list of vm guests that are powered off
    Inventory list of vm guests that are powered off

---
+++ poweredoffinventory.ps1
@@ -0,0 +1,34 @@
+﻿Connect-VIServer
+$Report = @()
+$VMs = get-vm |Where-object {$_.powerstate -eq "poweredoff"}
+$Datastores = Get-Datastore | select Name, Id
+Get-VIEvent -Entity $VMs -MaxSamples ([int]::MaxValue) |
+
+where {$_ -is [VMware.Vim.VmPoweredOffEvent]} |
+
+Group-Object -Property {$_.Vm.Name} | %{
+
+  $lastPO = $_.Group | Sort-Object -Property CreatedTime -Descending | Select -First 1
+
+  $vm = Get-VM -Name $_.Name
+
+  $row = '' | select VMName,Powerstate,OS,Host,Cluster,Datastore,NumCPU,MemMb,DiskGb,PowerOFF
+
+    $row.VMName = $vm.Name
+    $row.Powerstate = $vm.Powerstate
+    $row.OS = $_.Guest.OSFullName
+    $row.Host = $vm.VMHost.name
+    $row.Cluster = $vm.VMHost.Parent.Name
+    $row.Datastore = ($Datastores | where {$_.ID -match (($vm.Datastore | Select -First 1) | Select Value).Value} | Select Name).Name
+    $row.NumCPU = $vm.NumCPU
+    $row.MemMb = $vm.MemoryMB
+    $row.DiskGb = ((($vm.HardDisks | Measure-Object -Property CapacityKB -Sum).Sum * 1KB / 1GB),2)
+    $row.PowerOFF = $lastPO.CreatedTime
+    $row.UserName = $lastPO.UserName
+
+    $report += $row
+
+}
+
+$report | Sort Name | Export-Csv -Path "C:\inventory_powered_off_vm.csv" -NoTypeInformation -UseCulture
+disconnect-viserver * -confirm:$false
