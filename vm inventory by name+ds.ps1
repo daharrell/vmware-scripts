68e4b35a6dd5c6b42307fb440bd1c85594dfffca
daharrell <daharrell@users.noreply.github.com>
2017-07-09 14:23:40 -0400

    Display VM inventory by guest name and datastore names
    Display VM inventory by guest name and datastore names

--- vm inventory by name+ds.ps1
+++ vm inventory by name+ds.ps1
@@ -1,1 +1,2 @@
-﻿Get-VM | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}} |Export-Csv C:\inventory_report.csv -NoTypeInformation -UseCulture
+Connect-VIServer
+Get-VM | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}} |Export-Csv C:\inventory_report.csv -NoTypeInformation -UseCulture
