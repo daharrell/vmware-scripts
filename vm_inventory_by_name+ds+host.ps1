c274516327b4b6e7746b5774fd8b6c2b96d5fbbe
Dan Harrell <daharrell@users.noreply.github.com>
2018-02-08 11:18:05 -0500

    Update vm_inventory_by_name+ds+host.ps1

--- vm_inventory_by_name+ds+host.ps1
+++ vm_inventory_by_name+ds+host.ps1
@@ -1,2 +1,3 @@
-﻿Connect-VIServer
+#Written by Dan Harrell
+Connect-VIServer
 Get-VM | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Host";E={$_.VMHost}} |Export-Csv C:\inventory_report_name_ds_host.csv -NoTypeInformation -UseCulture
