d6d4a16cd4632219bf78fc602d502fb66fd31400
Dan Harrell <daharrell@users.noreply.github.com>
2018-02-10 05:06:03 -0500

    Update and rename Fetch_list_of_nonshared_datastores to Fetch_list_of_nonshared_datastores.ps1

--- Fetch_list_of_nonshared_datastores.ps1
+++ Fetch_list_of_nonshared_datastores.ps1
@@ -1,1 +1,3 @@
+Connect_Viserver    
 Get-Datacenter "DC NAME" | get-datastore|get-view | ? {$_.summary.multiplehostaccess -eq $false} | select @{n=”DS: Name”;e={$_.name}},@{n=”DS: Shared”;e={$_.summary.multiplehostaccess}}
+Disconnect-viserver * -confirm:$false
