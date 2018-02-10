2eb300aeae56041225644091b1b335c6003441c1
Dan Harrell <daharrell@users.noreply.github.com>
2018-02-10 05:08:10 -0500

    Update vm inventory by name+ds.ps1

--- vm inventory by name+ds.ps1
+++ vm inventory by name+ds.ps1
@@ -1,2 +1,3 @@
 Connect-VIServer
 Get-VM | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}} |Export-Csv C:\inventory_report.csv -NoTypeInformation -UseCulture
+Disconnect-viserver * -confirm:$false
