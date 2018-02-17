201757f8859f9b99a48d7138a16578b16d78a949
Dan Harrell <daharrell@users.noreply.github.com>
2018-02-17 09:39:28 -0500

    Update and rename disable_nic_by_OS_ type.ps1 to PowerCli/disable_nic_by_OS_ type.ps1

--- PowerCli/disable_nic_by_OS_ type.ps1
+++ PowerCli/disable_nic_by_OS_ type.ps1
@@ -1,2 +1,3 @@
 Connect-VIServer
 Get-VM | Where{$_.Guest.OSFullName -match 'windows'}|Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -Confirm:$false
+Disconnect-viserver * -confirm:$false
