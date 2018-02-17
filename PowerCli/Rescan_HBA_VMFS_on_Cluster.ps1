98d176344e9e886aadf78896122bc1a4d24f1c6a
Dan Harrell <daharrell@users.noreply.github.com>
2018-02-17 09:37:08 -0500

    Update and rename Rescan_HBA_VMFS_on_Cluster.ps1 to PowerCli/Rescan_HBA_VMFS_on_Cluster.ps1

---
+++ PowerCli/Rescan_HBA_VMFS_on_Cluster.ps1
@@ -0,0 +1,4 @@
+#Rescan HBA and VMFS on all hosts within a vCenter cluster
+Connect-Viserver
+Get-Cluster "ClusterName"| get-vmhost | Get-vmhoststorage -rescanallhba -rescanvmfs
+Disconnect-viserver * -confirm:$false 
--- Rescan_HBA_VMFS_on_Cluster.ps1
+++
@@ -1,3 +0,0 @@
- #Rescan HBA and VMFS on all hosts within a vCenter cluster
- Connect-Viserver
- Get-Cluster "ClusterName"| get-vmhost | Get-vmhoststorage -rescanallhba -rescanvmfs
