8c14f5c862f8023b89204cdfe61e733774a5f7ed
Dan Harrell <daharrell@users.noreply.github.com>
2018-11-09 15:29:35 -0500

    Update vcenter_inventory_by_name_cluster_os.ps1

--- PowerCli/vcenter_inventory_by_name_cluster_os/vcenter_inventory_by_name_cluster_os.ps1
+++ PowerCli/vcenter_inventory_by_name_cluster_os/vcenter_inventory_by_name_cluster_os.ps1
@@ -1,1 +1,4 @@
+#Written by Dan Harrell
+Connect-VIServer
 get-vm | Select Name,@{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}},@{N="Running OS";E={$_.Guest.OsFullName}}, @{N="Powered On";E={ $_.PowerState -eq “PoweredOn”}}, @{N="Host";E={$_.VMHost}} |Export-Csv C:\users\Dan_Harrell\Desktop\inventory_report_name_ds_host.csv -NoTypeInformation -UseCulture
+Disconnect-viserver * -confirm:$false
