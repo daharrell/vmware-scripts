06a7e4066a709a5b5ccc77405d560e5955297e03
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-20 11:21:02 -0400

    Update GetVMtoolsStatus.ps1

--- PowerCli/GetVMtoolsStatus/GetVMtoolsStatus.ps1
+++ PowerCli/GetVMtoolsStatus/GetVMtoolsStatus.ps1
@@ -1,2 +1,5 @@
 #Get VMTools status with VM Name, OS, and Powerstate
-Get-VM | Get-View | Select-Object @{N="VM Name";E={$_.Name}},@{Name="VMware Tools";E={$_.Guest.ToolsStatus}},@{Name="OS";E={$_.Guest.GuestFullName}},@{Name="PowerState";E={$_.Guest.GuestState}}| Export-CSV C:\VMwareToolsState.csv
+#Authored by Dan Harrell
+Connect-Viserver
+Get-VM | Get-View | Select-Object @{N="VM Name";E={$_.Name}},@{Name="VMware Tools";E={$_.Guest.ToolsStatus}},@{Name="OS";E={$_.Guest.GuestFullName}},@{Name="PowerState";E={$_.Guest.GuestState}}| Export-Csv -path "c:\users\$env:USERNAME\desktop\vcenter_vmtools_status.csv" -NoTypeInformation -UseCulture
+Disconnect-viserver * -confirm:$false
