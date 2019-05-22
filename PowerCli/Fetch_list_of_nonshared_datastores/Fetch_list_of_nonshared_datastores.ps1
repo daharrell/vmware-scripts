845f83e88e3a83874b12ace119a04c4abc9c7e5d
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:35:53 -0400

    Update Fetch_list_of_nonshared_datastores.ps1

--- PowerCli/Fetch_list_of_nonshared_datastores/Fetch_list_of_nonshared_datastores.ps1
+++ PowerCli/Fetch_list_of_nonshared_datastores/Fetch_list_of_nonshared_datastores.ps1
@@ -2,6 +2,10 @@
 #Ex. Get-Datacenter "SomeDCName"
 #If you are unsure of what the Datacenter names are you can just run Get-Datacenter by itself to output a list
 #Authored by Dan Harrell
-Connect_Viserver    
+#Updated 5/22/2019
+
+$vcenter = "NameOrIPofVcenter"
+Connect_Viserver $vcenter 
+#Exports the list to your desktop
 Get-Datacenter | get-datastore|get-view | ? {$_.summary.multiplehostaccess -eq $false} | select @{n=”DS: Name”;e={$_.name}},@{n=”DS: Shared”;e={$_.summary.multiplehostaccess}} | Export-Csv -path "c:\users\$env:USERNAME\Desktop\FetchedSharedDatastoreList.csv" -NoTypeInformation -UseCulture
 Disconnect-viserver * -confirm:$false
