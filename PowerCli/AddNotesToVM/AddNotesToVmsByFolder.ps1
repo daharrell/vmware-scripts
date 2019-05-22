6e65dc21cd7016ec2abb5404b39e8e1dab08526d
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:15:56 -0400

    Update AddNotesToVmsByFolder.ps1

--- PowerCli/AddNotesToVM/AddNotesToVmsByFolder.ps1
+++ PowerCli/AddNotesToVM/AddNotesToVmsByFolder.ps1
@@ -1,2 +1,10 @@
-connect-viserver  vCenterNameORip
-Get-Datacenter DCNAME | Get-Folder Infected_DO_NOT_POWER_ON |Get-VM | Set-VM -Notes "$($_.Notes) Your Note here"
+#Variables to be entered here
+$vcentername = "NameOrIpOfVcenterGoesHere:
+$dcname = "NameOfTheVMdatacenterGoeshere"
+$folder = "NameOfFolderInVcenterGoesHere"
+#Connect to Vcenter
+connect-viserver $vcentername
+#Command to add notes to the specified folder go here
+Write-Host "Notes are being added to $folder in $dcname on vCenter $vcentername"
+Get-Datacenter $vcentername | Get-Folder $folder |Get-VM | Set-VM -Notes "$($_.Notes) Your Note here"
+Disconnect-viserver * -confirm:$false
