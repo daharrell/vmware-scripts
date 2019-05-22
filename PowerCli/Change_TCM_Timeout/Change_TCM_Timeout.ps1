3aded58df7faa34a7d099a4e9c272fa1fbfe6db3
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:30:52 -0400

    Update Change_TCM_Timeout.ps1

--- PowerCli/Change_TCM_Timeout/Change_TCM_Timeout.ps1
+++ PowerCli/Change_TCM_Timeout/Change_TCM_Timeout.ps1
@@ -1,5 +1,11 @@
-#Written by Dan Harrell
-#Sets the timeout of TCM to 300 seconds.
-Connect-VIServer
-Get-Datacenter "DC Name" | Get-vmhost -state Connected | Get-AdvancedSetting -Name 'UserVars.ESXiShellInteractiveTimeOut' | Set-AdvancedSetting -Value "300" -Confirm:$false
+#Sets the timeout of TCM to whatever the variable is set to, 
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+vcenter = "NameOrIPofVcenter"
+$dcname = "dcnameinsidevcenter"
+$tcmValue = "300"
+
+Connect-VIServer $vcenter
+Get-Datacenter $dcname | Get-vmhost -state Connected | Get-AdvancedSetting -Name 'UserVars.ESXiShellInteractiveTimeOut' | Set-AdvancedSetting -Value $tcmValue -Confirm:$false
 Disconnect-viserver * -confirm:$false
