239a5ac58528237d8691903977777d6f04951122
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:27:18 -0400

    Update ChangeAdvancedSetting.ps1

--- PowerCli/ChangeAdvancedSetting/ChangeAdvancedSetting.ps1
+++ PowerCli/ChangeAdvancedSetting/ChangeAdvancedSetting.ps1
@@ -1,3 +1,13 @@
-#This will allow you to change an advanced setting on all hosts in a cluster
-Get-Cluster -name "ClusterName"| Get-VMHOST | Get-AdvancedSetting -Name "SomeAdvancedSettingName" | Set-AdvancedSetting -Val
-ue "SomeValue"
+#This will allow you to change one advanced setting on all hosts in a cluster
+#Authored by Dan Harrell
+#Updated 5/22/2019
+#Set variables
+$cluster = "ClusterNameGoesHere"
+$advance = "SomeAdvancedSettingName"
+$advanceValue = "SomeValue"
+$vcenter = "NameOrIPofVcenter"
+
+Connect-Viserver $vcenter
+Write-Host "Changing $advance setting to $advanceValue for all VM's in $cluster cluster on vcenter $vcenter"
+Get-Cluster -name $cluster | Get-vmhost | Get-AdvancedSetting -Name $advance | Set-AdvancedSetting -Value $advanceValue
+Disconnect-viserver * -confirm:$false
