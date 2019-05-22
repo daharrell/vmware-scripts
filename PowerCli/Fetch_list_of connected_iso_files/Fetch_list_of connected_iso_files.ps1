50353b1be6a2f7c67eaafe9548563e4a521df922
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:34:46 -0400

    Update Fetch_list_of connected_iso_files.ps1

--- PowerCli/Fetch_list_of connected_iso_files/Fetch_list_of connected_iso_files.ps1
+++ PowerCli/Fetch_list_of connected_iso_files/Fetch_list_of connected_iso_files.ps1
@@ -1,3 +1,10 @@
-Connect-Viserver
-Get-Datacenter "DC NAME" | Get-VM | Get-CDDrive | where {$_.IsoPath} | Select Parent,Name,ConnectionState,IsoPath |Export-Csv C:\Your PATH\cdrom-inventory.csv -NoTypeInformation -UseCulture
+# Fetchs a list of ISO files that are currently connect to VM's from vCenter
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+$vcenter = "NameOrIPofVcenter"
+
+Connect-Viserver $vcenter
+#List gets exported to your desktop as a CSV
+Get-Datacenter | Get-VM | Get-CDDrive | where {$_.IsoPath} | Select Parent,Name,ConnectionState,IsoPath |Export-Csv -path "c:\users\$env:USERNAME\desktop\vcenter1_cdrom_inventory.csv" -NoTypeInformation -UseCulture
 Disconnect-viserver * -confirm:$false
