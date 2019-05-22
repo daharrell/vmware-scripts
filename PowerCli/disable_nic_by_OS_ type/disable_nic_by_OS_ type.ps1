f01fceab3b5c663161d9d855d1c39f07f679292c
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:56:28 -0400

    Update disable_nic_by_OS_ type.ps1

--- PowerCli/disable_nic_by_OS_ type/disable_nic_by_OS_ type.ps1
+++ PowerCli/disable_nic_by_OS_ type/disable_nic_by_OS_ type.ps1
@@ -1,3 +1,9 @@
-Connect-VIServer
+
+#Authored by Dan Harrell
+#Updated 5/22/2019
+#vcenter name or IP
+$vcenter = "NameOrIPofVcenter"
+
+Connect-VIServer $vcenter
 Get-VM | Where{$_.Guest.OSFullName -match 'windows'}|Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -Confirm:$false
 Disconnect-viserver * -confirm:$false
