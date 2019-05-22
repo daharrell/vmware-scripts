06b123172968d74ea17a1f4760a005d49d8a0101
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:58:10 -0400

    Update eject-cd-rom-iso.ps1

--- PowerCli/eject-cd-rom-iso/eject-cd-rom-iso.ps1
+++ PowerCli/eject-cd-rom-iso/eject-cd-rom-iso.ps1
@@ -1,3 +1,12 @@
-Connect-Viserver
-Get-Datacenter "DC Name" | Get-VM | Get-CDDrive | where {$_.IsoPath -ne $null} | Set-CDDrive -NoMedia -Confirm:$False 
+#Ejects all iso files for every vm within a vCenter Datacenter
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#vcenter name or IP
+$vcenter = "NameOrIPofVcenter"
+#Name of DC inside vcenter
+$dcname = "SomeDCName
+
+Connect-Viserver $vcenter
+Get-Datacenter $dcname | Get-VM | Get-CDDrive | where {$_.IsoPath -ne $null} | Set-CDDrive -NoMedia -Confirm:$False 
 Disconnect-viserver * -confirm:$false
