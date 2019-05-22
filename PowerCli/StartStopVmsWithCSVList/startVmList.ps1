8ade1175a891bab974101e2fcc4c9f3448a6a29f
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:49:16 -0400

    Update startVmList.ps1

--- PowerCli/StartStopVmsWithCSVList/startVmList.ps1
+++ PowerCli/StartStopVmsWithCSVList/startVmList.ps1
@@ -1,7 +1,15 @@
 #Add Name or IP to Connect-vcenter comman
 #Add path to where the csv is stored for import-csv command
-connect-viserver NameOrIpOfVcenter
-$vmlist = import-csv c:\someptahtto\vmlist.csv | select -ExpandProperty NAME
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#vcenter name or IP
+$vcenter = "NameOrIPofVcenter"
+#Path to csv file
+$path = "c:\some\path\to\vmlist.csv
+
+connect-viserver $vcenter
+$vmlist = import-csv $path | select -ExpandProperty NAME
 foreach ($vm in $vmlist) {
 Get-VM $vm | Where-Object {$_.powerstate -eq ‘PoweredOFF’} | Start-VM -Confirm:$false}
 Disconnect-VIServer -confirm:$false
