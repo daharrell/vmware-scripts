7b21d4d8f5d0a7fcb6282aa5b88589662f999e59
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 11:00:53 -0400

    Update timesync_vm_list.ps1

--- PowerCli/timesync_vm_list/timesync_vm_list.ps1
+++ PowerCli/timesync_vm_list/timesync_vm_list.ps1
@@ -1,3 +1,12 @@
-Connect-VIserver
-get-view -viewtype virtualmachine -Filter @{'Config.Tools.SyncTimeWithHost'='True'} | select name |Export-Csv C:\timesync.csv -NoTypeInformation -UseCulture
+#Pulls list of VM's that Sync Time with Host
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#vcenter name or IP
+$vcenter = "NameOrIPofVcenter"
+#exports list to current users desktop
+$pathToExport = “c:\users\$env:USERNAME\desktop\timesync.csv
+
+Connect-VIserver $vcenter
+get-view -viewtype virtualmachine -Filter @{'Config.Tools.SyncTimeWithHost'='True'} | select name |Export-Csv $pathToExport -NoTypeInformation -UseCulture
 Disconnect-viserver * -confirm:$false
