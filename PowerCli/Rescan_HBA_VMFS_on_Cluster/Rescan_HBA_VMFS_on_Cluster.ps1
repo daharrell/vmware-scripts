1a89aa496233df0f2b7461228d3269e79b58f666
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:45:31 -0400

    Update Rescan_HBA_VMFS_on_Cluster.ps1

--- PowerCli/Rescan_HBA_VMFS_on_Cluster/Rescan_HBA_VMFS_on_Cluster.ps1
+++ PowerCli/Rescan_HBA_VMFS_on_Cluster/Rescan_HBA_VMFS_on_Cluster.ps1
@@ -1,4 +1,11 @@
 #Rescan HBA and VMFS on all hosts within a vCenter cluster
-Connect-Viserver
-Get-Cluster "ClusterName"| get-vmhost | Get-vmhoststorage -rescanallhba -rescanvmfs
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#Variables need to be changed to work for your environment
+$vcenter = "NameOrIPofVcenter"
+$cluster = "ClusterNameGoesHere"
+
+Connect-Viserver $vcenter
+Get-Cluster $cluster get-vmhost | Get-vmhoststorage -rescanallhba -rescanvmfs
 Disconnect-viserver * -confirm:$false 
