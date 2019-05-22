5133cc69db350c1b0bf13d9bf7d41d21f678e949
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 11:06:16 -0400

    Update vcenter_inventory_by_name_cluster_os.ps1

--- PowerCli/vcenter_inventory_by_name_cluster_os/vcenter_inventory_by_name_cluster_os.ps1
+++ PowerCli/vcenter_inventory_by_name_cluster_os/vcenter_inventory_by_name_cluster_os.ps1
@@ -1,4 +1,11 @@
-#Written by Dan Harrell
-Connect-VIServer
-get-vm | Select Name,@{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}},@{N="Running OS";E={$_.Guest.OsFullName}}, @{N="Powered On";E={ $_.PowerState -eq “PoweredOn”}}, @{N="Host";E={$_.VMHost}} |Export-Csv C:\users\Dan_Harrell\Desktop\inventory_report_name_ds_host.csv -NoTypeInformation -UseCulture
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#vcenter name or IP
+$vcenter = "NameOrIPofVcenter"
+#Path to export CSV
+$PathToExport = "c:\users\$env:USERNAME\desktop\inventory_report_name_ds_host.csv"
+
+Connect-VIServer $vcenter
+get-vm | Select Name,@{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}},@{N="Running OS";E={$_.Guest.OsFullName}}, @{N="Powered On";E={ $_.PowerState -eq “PoweredOn”}}, @{N="Host";E={$_.VMHost}} | Export-Csv $PathToExport -NoTypeInformation -UseCulture
 Disconnect-viserver * -confirm:$false
