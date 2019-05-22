a936fea9beda4b8232d02897163bd6a748a1d46e
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-22 10:53:10 -0400

    Update Stop_SSH_Shell_and_Set_to_Manual_StartupStop_SSH_Shell_and_Set_to_Manual_Startup.ps1

--- PowerCli/Stop_SSH_Shell_and_Set_to_Manual_StartupStop_SSH_Shell_and_Set_to_Manual_Startup/Stop_SSH_Shell_and_Set_to_Manual_StartupStop_SSH_Shell_and_Set_to_Manual_Startup.ps1
+++ PowerCli/Stop_SSH_Shell_and_Set_to_Manual_StartupStop_SSH_Shell_and_Set_to_Manual_Startup/Stop_SSH_Shell_and_Set_to_Manual_StartupStop_SSH_Shell_and_Set_to_Manual_Startup.ps1
@@ -3,18 +3,26 @@
 #a single host then delete Get-Datacenter "DC Name" |  then add the host name after Get-vmhost.
 #Comment out which ever commands you dont want to run if you're trying to run this as actual script instead of just using the oneliner
 #in powercli.
-Connect-Viserver
+
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#vcenter name or IP
+$vcenter = "NameOrIPofVcenter"
+$dcname = "Name of Datacenter inside vcenter"
+
+Connect-Viserver $vcenter
 
 ###Change startup policy of SSH to Manual
-#Get-Datacenter "DC Name" | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM-SSH"} | set-vmhostservice -policy "Off"
+#Get-Datacenter $dcname | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM-SSH"} | set-vmhostservice -policy "Off"
 
 ###Change startup policy of ESXI Shell - TSM to Manual
-#Get-Datacenter "DC Name" | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM"} | set-vmhostservice -policy "Off"
+#Get-Datacenter $dcname | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM"} | set-vmhostservice -policy "Off"
 
 ###Stop of ESXI SHell -TSM service
-#Get-Datacenter "DC Name" | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM"} | Stop-VMHostService -Confirm:$false
+#Get-Datacenter $dcname | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM"} | Stop-VMHostService -Confirm:$false
 
 ###Stop of SSH service
-#Get-Datacenter "DC Name" | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM-SSH"} | Stop-VMHostService -Confirm:$false
+#Get-Datacenter $dcname | Get-vmhost -state Connected | get-vmhostservice | where-object {$_.key -eq "TSM-SSH"} | Stop-VMHostService -Confirm:$false
 
 Disconnect-viserver * -confirm:$false
