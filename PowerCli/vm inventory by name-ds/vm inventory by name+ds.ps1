a9dda80aa2c2aadbb3ce1b5f790d8ad69039ff8a
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-23 09:11:16 -0400

    Update vm inventory by name+ds.ps1

--- PowerCli/vm inventory by name-ds/vm inventory by name+ds.ps1
+++ PowerCli/vm inventory by name-ds/vm inventory by name+ds.ps1
@@ -1,3 +1,13 @@
-Connect-VIServer
-Get-VM | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}} |Export-Csv C:\inventory_report.csv -NoTypeInformation -UseCulture
+#Authored by Dan Harrell
+#Updated 5/22/2019
+
+#vcenter name or IP
+$vcenter = "vcenter1"
+#Path to export CSV
+$PathToExport = "c:\users\$env:USERNAME\desktop\" + $vcenter+"_inventory_report.csv"
+
+Connect-VIServer $vcenter
+
+Get-VM | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}} |Export-Csv -path $PathToExport -NoTypeInformation -UseCulture
+
 Disconnect-viserver * -confirm:$false
