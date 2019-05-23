4ccbb5303e563237a1fb2d01552c9225022c495b
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-23 09:02:36 -0400

    Update GetVMtoolsStatus.ps1

--- PowerCli/GetVMtoolsStatus/GetVMtoolsStatus.ps1
+++ PowerCli/GetVMtoolsStatus/GetVMtoolsStatus.ps1
@@ -3,8 +3,9 @@
 #Updated 5/22/2019
 
 $vcenter = "NameOrIPofVcenter"
+$PathToExport = "c:\users\$env:USERNAME\desktop\" + $vcenter+"_vmtools_status.csv"
 
 Connect-Viserver $vcenter
 #Exports status report to your desktop
-Get-VM | Get-View | Select-Object @{N="VM Name";E={$_.Name}},@{Name="VMware Tools";E={$_.Guest.ToolsStatus}},@{Name="OS";E={$_.Guest.GuestFullName}},@{Name="PowerState";E={$_.Guest.GuestState}}| Export-Csv -path "c:\users\$env:USERNAME\desktop\vcenter_vmtools_status.csv" -NoTypeInformation -UseCulture
+Get-VM | Get-View | Select-Object @{N="VM Name";E={$_.Name}},@{Name="VMware Tools";E={$_.Guest.ToolsStatus}},@{Name="OS";E={$_.Guest.GuestFullName}},@{Name="PowerState";E={$_.Guest.GuestState}}| Export-Csv -path $PathToExport -NoTypeInformation -UseCulture
 Disconnect-viserver * -confirm:$false
