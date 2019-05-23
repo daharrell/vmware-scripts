2e3cd4296407303ad9792cfab45a361e3bdcc33a
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-23 10:11:17 -0400

    Update vm_usage_stats.ps1

--- PowerCli/vm_usages_stats/vm_usage_stats.ps1
+++ PowerCli/vm_usages_stats/vm_usage_stats.ps1
@@ -2,7 +2,7 @@
 #Authored by Dan Harrell
 #Created 5/23/2019
 
-$vcenter = "vcenter1"
+$vcenter = "NameorIpofVcenterServer"
 $pathToExport = "c:\users\$env:USERNAME\desktop\" + $vcenter + "_vm_stats.csv"
 connect-viserver $vcenter
 Get-VM | Where {$_.PowerState -eq "PoweredOn"} | Select Name, NumCpu, MemoryMB,@{N="Cpu.UsageMhz.Average";E={[Math]::Round((($_ |Get-Stat -Stat cpu.usagemhz.average -Start (Get-Date).AddHours(-24)-IntervalMins 5 -MaxSamples (12) |Measure-Object Value -Average).Average),2)}},
