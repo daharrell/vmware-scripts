28b11302928bb96959ea665a7eca983b95d093ba
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-23 08:55:44 -0400

    Update vcenter_inventory_by_name_cluster_os.ps1

--- PowerCli/vcenter_inventory_by_name_cluster_os/vcenter_inventory_by_name_cluster_os.ps1
+++ PowerCli/vcenter_inventory_by_name_cluster_os/vcenter_inventory_by_name_cluster_os.ps1
@@ -4,7 +4,7 @@
 #vcenter name or IP
 $vcenter = "NameOrIPofVcenter"
 #Path to export CSV
-$PathToExport = "c:\users\$env:USERNAME\desktop\inventory_report_name_ds_host.csv"
+$PathToExport = "c:\users\$env:USERNAME\desktop\" + $vcenter+ "_inventory_report_name_ds_host.csv"
 
 Connect-VIServer $vcenter
 get-vm | Select Name,@{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}},@{N="Running OS";E={$_.Guest.OsFullName}}, @{N="Powered On";E={ $_.PowerState -eq “PoweredOn”}}, @{N="Host";E={$_.VMHost}} | Export-Csv $PathToExport -NoTypeInformation -UseCulture
