7227de99e6d3611c7074d1ebba22adcab7d7aaa4
Dan Harrell <daharrell@users.noreply.github.com>
2019-05-23 09:13:44 -0400

    Update vm inventory by name+ds.ps1

--- PowerCli/vm inventory by name-ds/vm inventory by name+ds.ps1
+++ PowerCli/vm inventory by name-ds/vm inventory by name+ds.ps1
@@ -2,7 +2,7 @@
 #Updated 5/22/2019
 
 #vcenter name or IP
-$vcenter = "vcenter1"
+$vcenter = "nameofvcenter"
 #Path to export CSV
 $PathToExport = "c:\users\$env:USERNAME\desktop\" + $vcenter+"_inventory_report.csv"
 
