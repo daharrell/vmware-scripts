11b1257ab8f502d329fd644692daa17f64a19ceb
Dan Harrell <daharrell@users.noreply.github.com>
2019-07-01 12:54:12 -0400

    Create CategoryCleaner.ps1

---
+++ PowerCli/CategoryAndTagSync/CategoryCleaner.ps1
@@ -0,0 +1,15 @@
+# Categories
+$PathToExportCleanCats = "C:\Path\to\Combined\vCenterCats.csv" 
+$getFirstLine = $true
+get-childItem "C:\Users\da287460\Desktop\TagCollector\category\*.csv" | foreach {
+    $filePath = $_
+    $lines = Get-Content $filePath  
+    $linesToWrite = switch($getFirstLine) {
+           $true  {$lines}
+           $false {$lines | Select -Skip 0}
+    }
+    $getFirstLine = $false
+    Add-Content "$PathToExportCleanCats" $linesToWrite
+    }
+$sortDupTag = Import-Csv -Path $PathToExportCleanCats | sort Name -Unique
+$sortDupTag | Export-Csv -Path $PathToExportCleanCats -NoTypeInformation -UseCulture
