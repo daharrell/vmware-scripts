826298c8f1a31582e32b3d29880a2fed4afd8d84
Dan Harrell <daharrell@users.noreply.github.com>
2019-10-14 20:46:41 -0400

    Update AddNotesToVmsByFolder.ps1

--- PowerCli/AddNotesToVM/AddNotesToVmsByFolder.ps1
+++ PowerCli/AddNotesToVM/AddNotesToVmsByFolder.ps1
@@ -1,14 +1,15 @@
 #Adds notes to the notes field for vm's
-#Authored by Dan Harrell
+#This will overwrite existing notes
+##Authored by Dan.Harrell - Dan.Harrell@HF.org
 #Updated 5/22/2019
 
 #Variables to be entered here
-$vcentername = "NameOrIpOfVcenterGoesHere:
+$vcentername = "NameOrIpOfVcenterGoesHere"
 $dcname = "NameOfTheVMdatacenterGoeshere"
 $folder = "NameOfFolderInVcenterGoesHere"
 #Connect to Vcenter
 connect-viserver $vcentername
 #Command to add notes to the specified folder go here
 Write-Host "Notes are being added to $folder in $dcname on vCenter $vcentername"
-Get-Datacenter $vcentername | Get-Folder $folder |Get-VM | Set-VM -Notes "$($_.Notes) Your Note here"
+Get-Datacenter $vcentername | Get-Folder $folder | Get-VM | Set-VM -Notes "$($_.Notes) Your Note here"
 Disconnect-viserver * -confirm:$false
